7526827632e230a7630dda7a13b0b3353411a0b2
henri <henrieduardo72@rede.ulbra.br>
2021-03-28 15:23:35 -0300

    qualquer coisa

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const Bot = new twit({
   consumer_secret: process.env.API_SECRET_KEY,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000,
 });
 
 function BotInit() {
