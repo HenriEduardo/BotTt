968ea9e31f759daef01f2483786be7a59343582a
henri <henrieduardo72@rede.ulbra.br>
2021-03-28 15:28:13 -0300

    j

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static(__dirname + '/bot-twitter'));
 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/bot-twitter/index.js');
+    res.sendFile(__dirname + '/bot-twitter/index.html');
 });
 
 app.listen(PORT, () =>{
