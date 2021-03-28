adde5efa24b453e32d46d2d9e94c75979a4e67c3
henri <henrieduardo72@rede.ulbra.br>
2021-03-28 14:09:17 -0300

    teste final

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log('Listening on ${ PORT }'))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 var twit = require('twit');
 
