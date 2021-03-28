79a98bb28e0bbd1708f83a3bf6f652e7b9305e4c
henri <henrieduardo72@rede.ulbra.br>
2021-03-28 13:25:36 -0300

    commit final

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-
-app.use(express.static(__dirname + '/'));
-app.get('*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './src/index.html'));
-});
-app.listen(process.env.PORT || 8080);
 
 var twit = require('twit');
 
